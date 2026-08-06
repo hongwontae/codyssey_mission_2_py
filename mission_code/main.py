f96b13f23a1e0a84071032b738bad7d69911f99b
dnjsxoghd <feelchok1234@gmail.com>
2026-08-06 17:40:35 +0900

    update

--- mission_code/main.py
+++ mission_code/main.py
@@ -1,4 +1,10 @@
 from quiz_game import QuizGame
 
+# __init__ 생성자 함수가 실행
+# QuizGame은 클래스 호출 -> 객체가 만들어집니다.
+# 객체란? -> 클래스는 설계도, 설계도를 통해서 만들어진 blueprint
+# 같은 클래스로 만든 객체라도 서로 영향을 받지 않는다.
 game = QuizGame()
+
+
 game.run()
